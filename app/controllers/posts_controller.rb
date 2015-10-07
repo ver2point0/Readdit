3c21d6b1a1ac9fca20400e56bacaab36450b3f0b
John McIntosh II <jmci10@hotmail.com>
2015-10-07 04:36:27 +0000

    Checkpoint 34 complete, intro to crud

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+  # post routes for creating, updating, viewing, deleting instances of post
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
