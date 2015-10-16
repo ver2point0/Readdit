0a3fa0dbcba6d0bca877a1f009a3390ba2b725b9
John McIntosh II <jmci10@hotmail.com>
2015-10-16 20:37:53 +0000

    Assignment 35 complete, validations

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Topic, type: :model do
   
   it { should have_many(:posts) }
   
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
+  
   describe "attributes" do
     it "should respond to name" do
       expect(topic).to respond_to(:name)    
