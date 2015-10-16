17095ded20934d8c34b017b80b95e5fa935942d9
John McIntosh II <jmci10@hotmail.com>
2015-10-16 05:34:20 +0000

    Checkpoint 35 complete, validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe Post, type: :model do
   
   it { should belong_to(:topic) }
   
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
+  
   describe "attributes" do
     
     it "should respond to title" do
