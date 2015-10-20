c44bf8a8a4c3de9aba757e3e01213dc9d34fe02e
John McIntosh II <jmci10@hotmail.com>
2015-10-20 00:02:44 +0000

    Assignment 37 complete authentication signing up

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
-
+  post 'users/confirm' => 'users#confirm'
+  
   root to: 'welcome#index'
 end
