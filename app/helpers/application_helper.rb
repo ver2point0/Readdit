49ed17c6314c72b2d168f80d5073c59e0286e815
John McIntosh II <jmci10@hotmail.com>
2015-10-20 05:28:54 +0000

    assignment 38 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
+    
     def form_group_tag(errors, &block)
      if errors.any?
        content_tag :div, capture(&block), class: 'form-group has-error'
      else
        content_tag :div, capture(&block), class: 'form-group'
      end
+    end
+    
+    def avatar_url(user)
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase    
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
     end
 end
