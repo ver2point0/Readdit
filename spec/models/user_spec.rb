72c85d9c7d171c56c0893e9af5e7b5f075829fea
John McIntosh II <jmci10@hotmail.com>
2015-10-24 08:07:54 +0000

    checkpoint 40 partially complete, authorization

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,48 @@ RSpec.describe User, type: :model do
     it "should respond to email" do
       expect(user).to respond_to(:email)
     end
+    
+    it "should respond to role" do
+      expect(user).to respond_to(:role)  
+    end
+    
+    it "should respond to admin?" do
+      expect(user).to respond_to(:admin?)  
+    end
+    
+    it "should respond to member?" do
+      expect(user).to respond_to(:member?)  
+    end
+  end
+  
+  describe "roles" do
+    it "should be member by default" do
+      expect(user.role).to eql("member")
+    end
+    
+    context "member user" do
+      it "should return true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+      
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey  
+      end
+    end
+    
+    context "admin user" do
+      before do
+        user.admin!
+      end
+      
+      it "should return false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+      
+      it "should return true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
+    end
   end
   
   describe "invalid user" do
