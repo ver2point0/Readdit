4da5c2a9e248503859c385735e55e2cbc86147e8
John McIntosh II <jmci10@hotmail.com>
2015-11-05 09:27:07 +0000

    assignment 44 complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,5 @@
 class FavoriteMailer < ApplicationMailer
-    default from: "jmci10@hotmail.com"
+    default from: "youremail.com"
     
     def new_comment(user, post, comment)
       
@@ -12,5 +12,16 @@ class FavoriteMailer < ApplicationMailer
       @comment = comment 
       
       mail(to: user.email, subject: "New comment on #{post.title}")
+    end
+    
+    def new_post(post)
+      
+      headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+      headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+      headers["References"] = "<post/#{post.id}@your-app-name.example>"
+      
+      @post = post
+      
+      mail(to: post.user.email, subject: "You are following, #{post.title}!")
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,8 @@ class Post < ActiveRecord::Base
     has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
     
+    after_create :create_favorite
+    
     default_scope { order('rank DESC') }
     
     validates :title, length: { minimum: 5 }, presence: true
@@ -32,6 +34,11 @@ class Post < ActiveRecord::Base
       age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
       new_rank = points + age_in_days
       update_attribute(:rank, new_rank)
+    end
+    
+    def create_favorite
+      Favorite.create(post: self, user: self.user)
+      FavoriteMailer.new_post(self).deliver_now
     end
     
 end
