9ff3d2d2ca003b8b8a5e441d713c72c7af34561f
John McIntosh II <jmci10@hotmail.com>
2015-11-06 21:57:38 +0000

    checkpoin 46 complete, private topics

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -4,6 +4,7 @@ include SessionsHelper
 
 RSpec.describe TopicsController, type: :controller do
   let(:my_topic) { create(:topic) }
+  let(:my_private_topic) { create(:topic, public: false) }  
   
   context "guest" do
     
@@ -17,9 +18,19 @@ RSpec.describe TopicsController, type: :controller do
         get :index
         expect(assigns(:topics)).to eq([my_topic])
       end
+      
+      it "does not include private topics in @topics" do
+        get :index
+        expect(assigns(:topics)).not_to include(my_private_topic)
+      end
     end
   
     describe "GET show" do
+      it "redirects from private topics" do
+        get :show, {id: my_private_topic.id}
+        expect(response).to redirect_to(new_session_path)
+      end
+      
       it "returns http success" do
         get :show, {id: my_topic.id}
         expect(response).to have_http_status(:success)
@@ -91,7 +102,7 @@ RSpec.describe TopicsController, type: :controller do
       
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
       end
     end
   
@@ -166,7 +177,7 @@ RSpec.describe TopicsController, type: :controller do
       
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
       end
     end
   
