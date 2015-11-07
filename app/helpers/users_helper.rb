38c78b2a9cde12c004893978e6a5555e4ea3be4b
John McIntosh II <jmci10@hotmail.com>
2015-11-07 16:54:33 +0000

    assignment 45 WIP

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  
+  def user_comments?(user)
+     user.comments.any? ? "Comments" : "#{user.name} has not submitted any comments yet."
+  end
+  
+  def user_posts?(user)
+    user.posts.any? ? "Posts" : "#{user.name} has not submitted any posts yet."
+  end
 end
