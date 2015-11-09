f725f802c1efc9a113e0fe8ee8db7b0ec2a738cc
John McIntosh II <jmci10@hotmail.com>
2015-11-09 06:16:19 +0000

    checkpoint 47 WIP, ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,14 +19,17 @@ class CommentsController < ApplicationController
     
     def destroy
       @post = Post.find(params[:post_id])
-      comment = @post.comments.find(params[:id])
+      @comment = @post.comments.find(params[:id])
       
-      if comment.destroy
+      if @comment.destroy
         flash[:notice] = "Comment was deleted."
-        redirect_to [@post.topic, @post]
       else
         flash[:error] = "Comment couldn't be deleted. Try again."
-        redirect_to [@post.topic, @post]
+      end
+      
+      respond_to do |format|
+        format.html
+        format.js
       end
     end
     
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe CommentsController, type: :controller do
     
     describe "POST create" do
       it "redirects the user to the sign in view" do
-        post :create, post_id: my_post.id, comment: {title: RandomData.random_sentence, body: RandomData.random_paragraph }
+        post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
         expect(response).to redirect_to(new_session_path)
       end
     end
     
     describe "DELETE destroy" do
       it "redirects the user to the sign in view" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id 
         expect(response).to redirect_to(new_session_path)
       end
     end
@@ -46,7 +46,7 @@ RSpec.describe CommentsController, type: :controller do
     
     describe "DELETE destroy" do
       it "redirects the user to the posts show view" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
         expect(response).to redirect_to([my_topic, my_post])
       end
     end
@@ -61,7 +61,7 @@ RSpec.describe CommentsController, type: :controller do
      
      describe "POST create" do
        it "increases the number of comments by 1" do
-         expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)    
+         expect{ post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
        end
        
        it "redirects to the post show view" do
@@ -72,14 +72,14 @@ RSpec.describe CommentsController, type: :controller do
      
      describe "DELETE destroy" do
        it "deletes the comment" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
          count = Comment.where({id: my_comment.id}).count
          expect(count).to eq(0)
        end
        
-       it "redirects to the post show view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         expect(response).to redirect_to [my_topic, my_post]
+       it "returns http success" do
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+         expect(response).to have_http_status(:success)
        end
      end
       
@@ -105,14 +105,14 @@ RSpec.describe CommentsController, type: :controller do
     
     describe "DELETE destroy" do
       it "deletes the comment" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
         count = Comment.where({id: my_comment.id}).count
         expect(count).to eq(0)
       end
       
-      it "redirects to the post show view" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
-        expect(response).to redirect_to [my_topic, my_post]
+      it "returns http success" do
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+        expect(response).to have_http_status(:success)
       end
     end
   
