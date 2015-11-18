a4c118a35b384b50c9cfb80a6cd6a09584535653
John McIntosh II <jmci10@hotmail.com>
2015-11-18 06:32:56 +0000

    checkpoint 49 WIP

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show, :create, :update]
       resources :topics, only: [:index, :show]
     end
   end
