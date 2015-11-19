0fec7183399d63fdf94ee0884750d9203dce746c
John McIntosh II <jmci10@hotmail.com>
2015-11-19 08:37:36 +0000

    checkpoint 49 complete, building an api: sending data

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
       resources :topics, only: [:index, :show]
+      resources :topics, except: [:edit, :new]
     end
   end
   
